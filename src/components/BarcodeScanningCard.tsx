e017d21d038534ef8949449d9ac98174284a3bae
Dalson <dalsontws@gmail.com>
2022-10-19 21:57:45 +0800

    feat: stop stream

--- src/components/BarcodeScanningCard.tsx
+++ src/components/BarcodeScanningCard.tsx
@@ -5,15 +5,18 @@ import BarcodeScannerComponent from "react-qr-barcode-scanner";
 type BarcodeScanningCardProps = {
   barcodeData: string;
   setBarcodeData: (arg: string) => void;
+  barcodeScanning: boolean;
   setBarcodeScanning: (arg: boolean) => void;
 };
 
 export const BarcodeScanningCard = (props: BarcodeScanningCardProps) => {
-  const { barcodeData, setBarcodeData, setBarcodeScanning } = props;
+  const { barcodeData, setBarcodeData, barcodeScanning, setBarcodeScanning } =
+    props;
 
   return (
     <Card sx={{ width: 345 }}>
       <BarcodeScannerComponent
+        stopStream={barcodeScanning}
         facingMode="environment"
         width={500}
         height={500}
